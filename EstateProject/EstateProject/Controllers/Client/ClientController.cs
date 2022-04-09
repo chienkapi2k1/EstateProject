bd62a052d8b4018f8714ea87aba2b2e74862d471
chienkapi2k1 <chienkapi2k1@gmail.com>
2022-04-09 12:51:31 +0700

    chien update edit images

--- EstateProject/EstateProject/Controllers/Client/ClientController.cs
+++ EstateProject/EstateProject/Controllers/Client/ClientController.cs
@@ -24,5 +24,30 @@ namespace EstateProject.Controllers.Client
             ViewBag.lstSanPham = db.building.ToList();
             return View(lstBuilding/*.ToPagedList(pagenumber, pagesize)*/);
         }
+
+        public ActionResult AllBuilding()
+        {
+            List<building> lstBuilding = db.building.OrderBy(n => n.name).ToList();
+            //int pagesize = 12; //số sản phẩm trong 1 trang
+            //int pagenumber = (page ?? 1);     // số trang
+            if (lstBuilding.Count == 0)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            ViewBag.lstSanPham = db.building.ToList();
+            return View(lstBuilding/*.ToPagedList(pagenumber, pagesize)*/);
+        }
+        public ViewResult BuildingDetail1(int id)
+        {
+            building sanpham = db.building.SingleOrDefault(n => n.id == id);
+            if (sanpham == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(sanpham);
+        }
+
     }
 }
