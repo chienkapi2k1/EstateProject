a2189f18a2590880aecd80e49a59e9cc9f7fd138
chienkapi2k1 <chienkapi2k1@gmail.com>
2022-04-10 22:20:00 +0700

    chien update contact frontend

--- EstateProject/EstateProject/Controllers/Client/ClientController.cs
+++ EstateProject/EstateProject/Controllers/Client/ClientController.cs
@@ -13,7 +13,7 @@ namespace EstateProject.Controllers.Client
         // GET: Client
         public ActionResult Index()
         {
-            List<building> lstBuilding = db.building.Where(n => n.note == "1").OrderBy(n => n.name).ToList();
+            List<building> lstBuilding = db.building.Where(n => n.levels == "1").OrderBy(n => n.name).ToList();
             //int pagesize = 12; //số sản phẩm trong 1 trang
             //int pagenumber = (page ?? 1);     // số trang
             if (lstBuilding.Count == 0)
@@ -40,13 +40,52 @@ namespace EstateProject.Controllers.Client
         }
         public ViewResult BuildingDetail1(int id)
         {
-            building sanpham = db.building.SingleOrDefault(n => n.id == id);
-            if (sanpham == null)
+            building building = db.building.SingleOrDefault(n => n.id == id);
+
+            //user idUser = db.user.Find(id);
+            //List<user> user = db.user.Where(n => n.role == "STAFF").ToList();
+            //var selectList = new SelectList(user, "id", "fullname", idUser.id);
+
+            //ViewData["user_id"] = selectList;
+
+            //ViewData["user_idCheck"] = building.user_id;
+
+            if (building == null)
             {
                 Response.StatusCode = 404;
                 return null;
             }
-            return View(sanpham);
+            return View(building);
+        }
+        [HttpGet]
+        public ActionResult AddContact(building building)
+        {
+            ViewBag.user_id = new SelectList(db.user.Where(n => n.role == "STAFF").ToList(), "id", "fullname");
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddContact([Bind(Include = "id, user_id, email, fullname, phone, title , status, messages, createddate, modifieddate")] contacts contact)
+        {
+            if (ModelState.IsValid)
+            {
+                contact.title = "ok";
+                contact.status = "0";
+                db.contacts.Add(contact);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            //List<user> user = db.user.OrderBy(n => n.fullname).ToList();
+            //var selectList = new SelectList(user, "id", "fullname", idUser.id);
+
+            //ViewData["user_id"] = selectList;
+            ViewBag.user_id = new SelectList(db.user.Where(n => n.role == "STAFF").ToList(), "id", "fullname"); //
+            return View( );
+        }
+
+        public PartialViewResult BrokerPartial()
+        {
+            return PartialView(db.user.Where(n => n.role == "STAFF").ToList());
         }
 
     }
---
+++ EstateProject/EstateProject/Models/Metadata/contacts.Metadata.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EstateProject.Models
+{
+    [MetadataTypeAttribute(typeof(contactsMetadata))]
+    public partial class contacts
+    {
+        internal sealed class contactsMetadata
+        {
+            public int id { get; set; }
+            public int? user_id { get; set; }
+
+            [StringLength(255)]
+            [Required(ErrorMessage = "Please enter data for this field")]
+            [EmailAddress(ErrorMessage = "Invalid Email Address")]
+            public string email { get; set; }
+
+            [StringLength(255)]
+            [Required(ErrorMessage = "Please enter data for this field")]
+            public string fullname { get; set; }
+
+            [StringLength(255)]
+            [Required(ErrorMessage = "Please enter data for this field")]
+            [DataType(DataType.PhoneNumber)]
+            [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number")]
+            public string phone { get; set; }
+
+            public string title { get; set; }
+
+            [StringLength(255)]
+            public string status { get; set; }
+
+            [Required(ErrorMessage = "Please enter data for this field")]
+            public string messages { get; set; }
+
+            public DateTime? createddate { get; set; }
+
+            public DateTime? modifieddate { get; set; }
+
+        }
+    }
+}
